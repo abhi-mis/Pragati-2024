43f84ed98adb0a527ac236b63c60b743ac2b5dc9
polar-coder <abpolar790@gmail.com>
2024-02-23 00:39:30 +0530

    F

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import styles from "./Loader.module.css"
+import React from 'react';
+import styles from './Loader.module.css';
+import pragatiImage from './pragati.png';
+
 const Loader = () => {
   return (
     <>
-    <div className={styles.loader_body}>
-    <div className={styles.spinner_container}>
-  
-</div>
-    <img src="./images/plinth.jpg" alt='Please-wait' className={styles.loader_plinth_img}></img>
-
-
-    </div>
+      <div className={styles.loader_body}>
+        <div className={styles.spinner_container}></div>
+        <img src={pragatiImage} alt="Please-wait" className={styles.loader_plinth_img} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
