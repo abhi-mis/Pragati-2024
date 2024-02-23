8a3d66ff3c91d4988eccf91fbc77305099df2362
rairamjee <110187607+rairamjee@users.noreply.github.com>
2024-02-23 14:50:18 +0530

    Added Data

--- src/components/Competitions/data.js
+++ src/components/Competitions/data.js
@@ -36,7 +36,7 @@ export const events = [
     "id": "1",
     "name": "Innovative Idea Contest",
     "club": "Innovation",
-    "participation": "TEAM SIZE : 3-6",
+    "participation": "TEAM SIZE : 4-6",
     "imgName": "./images/events/astronomy/astrohunt/image.jpg",
     "poster": "/images/events/astronomy/astrohunt/poster.jpg",
     "about": "The guidelines ask for articles in specific topics and a specific format for new ideas. Presentations should be in PowerPoint and include the problem, solution, and why the solution is unique and possible.",
@@ -67,10 +67,10 @@ export const events = [
     "id": "2",
     "name": "Innovative Model Competition",
     "club": "Innovation",
-    "participation": "TEAM SIZE : 1-3",
+    "participation": "TEAM SIZE : 1-4",
     "imgName": "./images/events/astronomy/astromemia/image.jpg",
     "poster": "/images/events/astronomy/astromemia/poster.jpg",
-    "about": "The article submission guidelines specify domains like healthcare, agriculture, robotics, renewable energy, IoT, mobile and web app development, smart education, environment sustainability, future mobility, sensors, VLSI, and more. Submissions must follow a structured format including title, domain, problem definition, theoretical background, implementation, solution uniqueness, and a presentation with specific conten",
+    "about": "The article submission guidelines specify domains like healthcare, agriculture, robotics, renewable energy, IoT, mobile and web app development, smart education, environment sustainability, future mobility, sensors, VLSI, and more. Submissions must follow a structured format including title, domain, problem definition, theoretical background, implementation, solution uniqueness, and a presentation with specific content",
     "timeline": "6mar",
     "contacts":
       [
@@ -98,7 +98,7 @@ export const events = [
     "id": "3",
     "name": "Technical Poster Presentation",
     "club": "Innovation",
-    "participation": "Team Participation",
+    "participation": "TEAM SIZE : 1-3",
     "imgName": "./images/events/astronomy/brahmaand/image.jpg",
     "poster": "/images/events/astronomy/brahmaand/poster.jpg",
     "about": "Brahmaand is an event where you present your frame of reference towards the problem statement and come with effective ways or solutions with the help of proven resources.",
@@ -126,157 +126,214 @@ export const events = [
     "id": "4",
     "name": "Codefiesta",
     "club": "Coding and Engineering Design",
-    "participation": "Solo Participation",
+    "participation": "TEAM SIZE : 2",
     "imgName": "./images/events/astronomy/dhoomketu/image.jpg",
     "poster": "/images/events/astronomy/dhoomketu/poster.jpg",
-    "about": "",
+    "about": "Code Fiesta: Where coding duos battle in a fast-paced quiz and intense problem-solving showdown. Master MCQs, conquer coding challenges in C, C++, or Java, and outpace competitors to emerge victorious. ",
     "timeline": "",
     "contacts":
       [
         {
-          "name": "xx",
-          "number": "xx",
+          "name": "SUKANTO ROY",
+          "number": "8910116881",
           "email": "xx",
         },
         {
-          "name": "xx",
-          "number": "+xx",
+          "name": "SRIJIT SARKAR",
+          "number": "9432902403",
           "email": "xx",
         },
         {
-          "name": "xx",
-          "number": "xx",
-          "email": "xx",
+          "name": "PRIYODEEP MUKHERJEE",
+          "number": "8981534701",
+          "email": "",
+        },
+        {
+          "name": "NAVANIL GHOSH",
+          "number": "9123792013",
+          "email": "",
+        },
+        {
+          "name": "SNEHA SINGH",
+          "number": "9007923300",
+          "email": "",
         },
       ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/FAkqG4HjvUfAe6rJA",
   },
   {
     "id": "6",
     "name": "Catia - Design Contest",
     "club": "Coding and Engineering Design",
-    "participation": "Solo Participation",
+    "participation": "TEAM SIZE : 2",
     "imgName": "./images/events/astronomy/starpix/image.jpg",
     "poster": "/images/events/astronomy/starpix/poster.jpg",
-    "about": "",
+    "about": "Gear up for a drafting duel in 'Design with AutoCAD/Catia'! This challenge invites pairs of aspiring engineers to unleash their design dexterity and architectural acumen. Armed only with traditional drafting tools—leave your cell phones behind—you'll race against time to sketch the future.",
     "timeline": "",
     "contacts":
       [
         {
-          "name": "",
-          "number": "",
+          "name": "JAYANT JOSHI",
+          "number": "7059633144",
 
         },
         {
-          "name": "",
-          "number": "",
-        },
-        {
-          "name": "",
-          "number": "",
-
+          "name": "RANGAN JANA",
+          "number": "9874456530",
         },
       ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/98u1WbwK1EXyUib98",
 
   },
   {
     "id": "7",
     "name": "Cicuit Design",
     "club": "Coding and Engineering Design",
-    "participation": "Solo Participation",
+    "participation": "TEAM SIZE : 1-3",
     "imgName": "",
     "poster": "",
-    "about": "",
+    "about": "Whether solo or in teams of up to three, you're tasked with engineering innovative circuits using provided analog and digital components. With a strict two-hour timeline, precision, and ingenuity are key. Early submissions earn time bonuses, useful for tiebreaks.",
     "timeline": "",
     "contacts":
       [
         {
-          "name": "",
-          "number": "",
+          "name": "SWAPNANIL SAHA",
+          "number": "6291326445",
           "email": "",
         },
         {
-          "name": "",
-          "number": "",
+          "name": "SOHINI DUTTA",
+          "number": "9831150237",
           "email": "",
+        },
+        {
+          "name": "ARITRA DALUI",
+          "number": "7439135764",
+          "email":"",
         },
       ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/VgKBsitFUWrTLRaYA",
   },
   {
     "id": "8",
     "name": "ML Mania",
     "club": "Coding and Engineering Design",
-    "participation": "Solo Participation",
+    "participation": "TEAM SIZE : 2",
     "imgName": "",
     "poster": "",
-    "about": "",
+    "about": "With a blend of quizzes and hands-on coding, teams of two will navigate the complexities of machine learning, from foundational theories to applied projects. Test your skills in Python, impress the judges with innovative solutions, and vie for the top spot in this machine learning showdown.",
     "timeline": "",
     "contacts":
       [
         {
-          "name": "",
-          "number": "",
+          "name": "SURANJANA SAHA",
+          "number": "7439440087",
         },
         {
-          "name": "",
-          "number": "",
+          "name": "SNEHA SINGH",
+          "number": "9007923300",
+        },
+        {
+          "name": "MANAS KUMAR MANNA",
+          "number": "7362970829",
+
         },
       ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/wnaJbMYLokwXKAf26",
   },
   {
     "id": "9",
     "name": "ENIGMA",
     "club": "Quiz and Mathematical Aptitude",
-    "participation": "Solo Participation",
+    "participation": "TEAM SIZE : 3",
     "imgName": "",
     "poster": "",
-    "about": "",
+    "about": "Dive into a world of puzzles, codes, and programming across four intense rounds. Unravel word mysteries, decode jigsaws, master 'C' programming, and navigate lab riddles. Only the sharpest minds will prevail. Are you ready to unlock the secrets and claim victory?",
     "timeline": "19 December 2022 - Online round concluded 29 January 2023 - Onsite Round",
     "contacts":
       [
         {
-          "name": "",
-          "number": "",
+          "name": "ABHISHEK MISHRA",
+          "number": "8887850034",
         },
         {
-          "name": "",
-          "number": "",
+          "name": "ATREYEE KAR",
+          "number": "8777721356",
+        },
+        {
+          "name": "SUCHI KUMARI",
+          "number": "6200254450",
+
+        },
+        {
+          "name": "RAMJEE RAI",
+          "number": "877728405",
+
         },
       ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/SvpFMbsXV3F5SF7UA",
   },
   {
     "id": "10",
     "name": "Math Mania",
     "club": "Quiz and Mathematical Aptitude",
-    "participation": "Team Participation",
+    "participation": "TEAM SIZE : 2",
     "imgName": "",
     "poster": "",
+    "about": "Engage in challenging math problems across two rounds, aiming for a total of 100 marks. Only top performers advance. Teamwork essential.",
     "timeline": "",
-    "contacts": [],
+    "contacts":
+      [
+        {
+          "name": "TANISHA GUPTA",
+          "number": "820550887",
+        },
+        {
+          "name": "ANUSHKA KAR",
+          "number": "9874702562",
+        },
+        {
+          "name": "ATREYEE KAR",
+          "number": "8777721356",
+        },
+      ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/qjMAfWjMPJ4qozA99",
     },
     {
     "id": "11",
     "name": "NFS - Most Wanted",
     "club": "Gaming",
-    "participation": "Solo/Team Participation",
+    "participation": "Solo Participation",
     "imgName": "",
     "poster": "",
-    "about": "",
+    "about": "Dive into the adrenaline-pumping world of NFS Most Wanted, where you outrun rivals and evade cops in heart-stopping street races. Claim the top spot on the blacklist in this ultimate pursuit of speed and freedom!",
     "timeline": "",
-    "contacts": [],
+    "contacts": [
+      {
+        "name": "RAMJEE RAI",
+        "number": "8777281405",
+      },
+      {
+        "name": "LOKESH NAHAR",
+        "number": "9910138904",
+      },
+      {
+        "name": "SWANODEEP BANERJEE",
+        "number": "99031443255",
+      },
+      {
+        "name": "JAYANT JOSHI",
+        "number": "7059633144",
+      },
+    ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/Crw4qmpUo1KQeeVp8",
     },
     {
     "id": "12",
@@ -285,64 +342,236 @@ export const events = [
     "participation": "Solo Participation",
     "imgName": "",
     "poster": "",
-    "about": "",
+    "about": " International (No club or custom teams allowed)Player changes and strategy changes are limited to 3 times during the game. (The changes before the start of first and second half are not included in this count.",
     "timeline": "",
-    "contacts": [],
+    "contacts": [
+      {
+        "name": "RAMJEE RAI",
+        "number": "8777281405",
+      },
+      {
+        "name": "LOKESH NAHAR",
+        "number": "9910138904",
+      },
+      {
+        "name": "ROHAN SAHA",
+        "number": "8647862863",
+      },
+      {
+        "name": "NAVNIL GHOSH",
+        "number": "9123792013",
+      },
+    ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/2tvg7B8B6MCHJAkKA",
     },
     {
     "id": "13",
     "name": "Robo Fifa",
     "club": "Robotics",
-    "participation": "Solo Participation",
+    "participation": "TEAM SIZE : 2-4",
     "imgName": "",
     "poster": "",
-    "about": "",
+    "about": "Design a robot which plays soccer. The robot would compete against others in 1-1 matches. Arena Specifications: -Will be SURPRISE.",
     "timeline": "",
-    "contacts": [],
+    "contacts": [
+      {
+        "name": "JAYANT JOSHI",
+        "number": "7059633144",
+      },
+      {
+        "name": "BITHIKA DAS",
+        "number": "97980009670",
+      },
+      {
+        "name": "BINIT KUMAR SHA",
+        "number": "9903296261",
+      },
+      {
+        "name": "JAGARAN BISWAS",
+        "number": "8926707701",
+      },
+      {
+        "name": "ARNAB BARDHAN",
+        "number": "7890743642",
+      },
+      {
+        "name": "ABHIJIT MUKHERJEE",
+        "number": "6291513319",
+      },
+      {
+        "name": "SUMAN DAS",
+        "number": "9051672668",
+      },
+      {
+        "name": "SUBHOJIT MAJUMDER",
+        "number": "8697154474",
+      },
+    ],
     "rulebook": "",
-    "link": "",
+    "link": "https://forms.gle/q6PhxwwG7Jhxrkeh6",
     },
     {
       "id": "14",
       "name": "Robo War",
       "club": "Robotics",
-      "participation": "Team Participation",
+      "participation": "TEAM SIZE : 2-4",
       "imgName": "",
       "poster": "",
-      "about": "",
+      "about": "Build a robot whose task is to push the opposing robot out of the circular ring or flip it over. Different robots compete one-on-one against each other in a knock- out tournament.",
       "timeline": "",
-      "contacts": [],
+      "contacts": [
+        {
+          "name": "JAYANT JOSHI",
+          "number": "7059633144",
+        },
+        {
+          "name": "BITHIKA DAS",
+          "number": "97980009670",
+        },
+        {
+          "name": "BINIT KUMAR SHA",
+          "number": "9903296261",
+        },
+        {
+          "name": "JAGARAN BISWAS",
+          "number": "8926707701",
+        },
+        {
+          "name": "ARNAB BARDHAN",
+          "number": "7890743642",
+        },
+        {
+          "name": "ABHIJIT MUKHERJEE",
+          "number": "6291513319",
+        },
+        {
+          "name": "SUMAN DAS",
+          "number": "9051672668",
+        },
+        {
+          "name": "SUBHOJIT MAJUMDER",
+          "number": "8697154474",
+        },
+      ],
       "rulebook": "",
-      "link": "",
+      "link": "https://forms.gle/rafPbWVw4Bpnkj4Y9",
       },
       {
       "id": "15",
       "name": "Chase the Maze",
       "club": "Robotics",
-      "participation": "Solo Participation",
+      "participation": "TEAM SIZE : 2",
       "imgName": "",
       "poster": "",
-      "about": "",
+      "about": "To build a robot that is able to navigate out of the maze in the quickest time possible from the designated starting point to the ending point. ",
       "timeline": "",
-      "contacts": [],
+      "contacts": [
+        {
+          "name": "JAYANT JOSHI",
+          "number": "7059633144",
+        },
+        {
+          "name": "BITHIKA DAS",
+          "number": "97980009670",
+        },
+        {
+          "name": "BINIT KUMAR SHA",
+          "number": "9903296261",
+        },
+        {
+          "name": "JAGARAN BISWAS",
+          "number": "8926707701",
+        },
+        {
+          "name": "ARNAB BARDHAN",
+          "number": "7890743642",
+        },
+        {
+          "name": "ABHIJIT MUKHERJEE",
+          "number": "6291513319",
+        },
+        {
+          "name": "SUMAN DAS",
+          "number": "9051672668",
+        },
+        {
+          "name": "SUBHOJIT MAJUMDER",
+          "number": "8697154474",
+        },
+      ],
       "rulebook": "",
-      "link": "",
+      "link": "https://forms.gle/TduFRVg2vXNfbeBw7",
 
       },
       {
       "id": "25",
       "name": "Line Follower",
       "club": "Robotics",
-      "participation": "Team Participation",
+      "participation": "TEAM SIZE : 2-4",
       "imgName": "",
       "poster": "",
-      "about": "",
+      "about": "To make a robot to follow a white line on a black background, without losing the line and navigating several 90 degree turns. The robot to complete the course in the 14 shortest period of time while accurately tracking the course line from start to end wins",
       "timeline": "",
-      "contacts": [],
+      "contacts": [
+        {
+          "name": "JAYANT JOSHI",
+          "number": "7059633144",
+        },
+        {
+          "name": "BITHIKA DAS",
+          "number": "97980009670",
+        },
+        {
+          "name": "BINIT KUMAR SHA",
+          "number": "9903296261",
+        },
+        {
+          "name": "JAGARAN BISWAS",
+          "number": "8926707701",
+        },
+        {
+          "name": "ARNAB BARDHAN",
+          "number": "7890743642",
+        },
+        {
+          "name": "ABHIJIT MUKHERJEE",
+          "number": "6291513319",
+        },
+        {
+          "name": "SUMAN DAS",
+          "number": "9051672668",
+        },
+        {
+          "name": "SUBHOJIT MAJUMDER",
+          "number": "8697154474",
+        },
+      ],
       "rulebook": "",
-      "link": "",
+      "link": "https://forms.gle/NWA94jrFdVq13dcA7",
       },
 
 ];
+
+
+// "contacts":
+//       [
+//         {
+//           "name": "ABHISHEK MISHRA",
+//           "number": "8887850034",
+//         },
+//         {
+//           "name": "ARKAPRAVO DUTTA",
+//           "number": "739125500",
+//         },
+//         {
+//           "name": "SUCHI KUMARI",
+//           "number": "6200254450",
+
+//         },
+//         {
+//           "name": "KINGSHUK SARKAR",
+//           "number": "954785966",
+
+//         },
+//       ],
